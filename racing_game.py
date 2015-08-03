7f22c6edd6befa1a1e4336bd4756f4a579afd23f
vlieftink <vincentlieftink@gmail.com>
2015-08-04 00:28:20 +0200

    Able to move the car now.

--- racing_game.py
+++ racing_game.py
@@ -20,15 +20,27 @@ def car(x,y):
 x = (display_width * 0.45)
 y = (display_height * 0.7)
 
+x_change = 0
 
-
-crashed = False
+crashed = False # Dont crash on me, okay?
 
 while not crashed:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x_change = -5
+            elif event.key == pygame.K_RIGHT:
+                x_change = 5
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                x_change = 0
+
+    x += x_change
+
 
     gameDisplay.fill(white)
     car(x,y)
